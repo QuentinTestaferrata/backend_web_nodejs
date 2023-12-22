bd7eeea036ac509feb668ca391ba47d931f0e3b4
QuentinTestaferrata <quentin.testaferrata@student.ehb.be>
2023-12-22 16:09:09 +0100

    Feature MongoDB connectie
    MongoDB is added to my project

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+
+
 const express = require('express');
 const app = express();
 const PORT = 3000;
-
 app.use(express.json());
+//DB
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://Quentin:Qr931768463@cluster0.7qxergy.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
 
 const taskRoutes = require('./routes/tasks');
 app.use('/tasks', taskRoutes);
