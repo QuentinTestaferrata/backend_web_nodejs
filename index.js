7b13b12769dfa162e03a11f569ce90a0bef6a31e
QuentinTestaferrata <quentin.testaferrata@student.ehb.be>
2024-01-15 20:26:56 +0100

    Feature: Added a gitignore file
    Finished project

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const PORT = 3000;
 app.use(express.json());
 //DB
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Quentin:Qr931768463@cluster0.7qxergy.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true })
+
+//Use mongoose.connect('mongodb+srv://Username:Password@cluster.xxxxx.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true })
+//To connect your own MongoDB and use your own cluster
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
